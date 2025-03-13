12749a5f4bb612b1ab80d6d28bf08fb13be58f76
JB <joeyjordanbarnes@hotmail.com>
2025-03-13 20:51:19 +0000

    Added todo.txt, formatted actions.rs

--- src/actions.rs
+++ src/actions.rs
@@ -1,22 +1,18 @@
-use std::process::Command;
 use std::io::Result;
+use std::process::Command;
 // use crate::Result;
 
 pub struct Action {
     pub open_file: fn(&str) -> Result<()>,
-    pub help: Help,
-    pub new_folder, 
+    // pub help: Help,
+    pub new_folder: Result<()>,
 }
 
 pub fn open_file(file_path: &str) -> Result<()> {
     if cfg!(target_os = "macos") {
-        Command::new("nvim")
-            .arg(file_path)
-            .spawn()?;
+        Command::new("nvim").arg(file_path).spawn()?;
     } else if cfg!(target_os = "linux") {
-        Command::new("$EDITOR")
-            .arg(file_path)
-            .spawn()?;
+        Command::new("$EDITOR").arg(file_path).spawn()?;
     } else {
         return Err(std::io::Error::new(
             std::io::ErrorKind::Other,
@@ -26,4 +22,3 @@ pub fn open_file(file_path: &str) -> Result<()> {
 
     Ok(())
 }
-
